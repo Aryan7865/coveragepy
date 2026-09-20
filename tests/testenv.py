8b7d10d699e5d9656344773a394faa2657c33096
Ned Batchelder <ned@nedbatchelder.com>
2026-09-20 14:13:38 -0400

    test: Hypothesis only likes pypy 3.11

--- tests/testenv.py
+++ tests/testenv.py
@@ -44,4 +44,4 @@ CAN_MEASURE_BRANCHES = env.PYBEHAVIOR.branch_right_left
 
 # Can we use Hypothesis?
 # As of 6.156.0, PyPy 3.10 is no longer supported.
-USE_HYPOTHESIS = env.PYVERSION >= (3, 11) or env.CPYTHON
+USE_HYPOTHESIS = env.CPYTHON or (env.PYVERSION[:2] == (3, 11))
